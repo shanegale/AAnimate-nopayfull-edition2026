22dd8d9eae3cde8c826acb3092bb0d437e3ea1d5
shanegale <shaneyeahboigale@hotmail.com>
2026-06-05 07:42:34 -0400

    Add files via upload

---
+++ import.hx
@@ -0,0 +1,18 @@
+#if !macro
+import swfty.utils.*;
+
+import swfty.Structure;
+import swfty.utils.MathUtils;
+import swfty.utils.File;
+
+import swfty.renderer.BaseSprite;
+import swfty.renderer.BaseLayer;
+import swfty.renderer.BaseText;
+import swfty.renderer.Sprite;
+import swfty.renderer.Layer;
+import swfty.renderer.Text;
+#end
+
+using StringTools;
+using Lambda;
+using swfty.utils.Tools;
